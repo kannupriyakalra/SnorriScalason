d39098fef5d7b59e1801556824825b58f17ac0c9
Noel Welsh <noel@noelwelsh.com>
2024-02-09 10:47:50 +0000

    Add a basic nav bar
    Non-functional but has some basic styling

--- frontend/src/main/scala/snorri/Main.scala
+++ frontend/src/main/scala/snorri/Main.scala
@@ -5,6 +5,7 @@ import tyrian.Html.*
 import tyrian.*
 
 import scala.scalajs.js.annotation.*
+import cats.syntax.group
 
 @JSExportTopLevel("Snorri")
 object Main extends TyrianIOApp[Msg, Model]:
@@ -21,10 +22,31 @@ object Main extends TyrianIOApp[Msg, Model]:
 
   def view(model: Model): Html[Msg] =
     div(
-      h1("Snorri"),
-      button(onClick(Msg.Decrement))("-"),
-      div(model.toString),
-      button(onClick(Msg.Increment))("+")
+      header(
+        nav(`class` := "sticky mx-auto px-2 sm:px-6 lg:px-8 bg-slate-800 text-gray-300 border-b-2 border-slate-700")(
+          div(`class` := "flex h-16 items-center justify-between")(
+            // Left group
+            div(
+              h5(
+                `class` := "font-extrabold hover:bg-slate-700 hover:text-white rounded-md border-2 border-slate-700 px-3 py-2"
+              )(
+                a(href := "/")("S")
+              )
+            ),
+            // Right group
+            div(`class` := "flex items-center justify-between")(
+              button(`class` := "hover:bg-slate-700 hover:text-white rounded-md px-3 py-2 mr-3")("Login"),
+              button(`class` := "hover:bg-slate-700 hover:text-white rounded-md px-3 py-2 mr-3")("Signup")
+            )
+          )
+        )
+      ),
+      main(`class` := "px-2 sm:px-6 lg:px-8 mt-4")(
+        h1(`class` := "text-3xl")(text("Snorri"), span(`class` := "font-extrabold")("Bookson")),
+        button(onClick(Msg.Decrement))("-"),
+        div(model.toString),
+        button(onClick(Msg.Increment))("+")
+      )
     )
 
   def subscriptions(model: Model): Sub[IO, Msg] =
