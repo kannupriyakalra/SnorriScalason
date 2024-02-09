39614d2f71438f80eb29f136bdd71a9d0ce5b705
Noel Welsh <noel@noelwelsh.com>
2024-02-09 10:47:12 +0000

    Tell tailwind to scan scala files
    Tell the tailwind command looks for CSS style names in Scala files. Amazingly
    this works.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/main/**/*.{html,js,ts,jsx,tsx}",
+    "./src/main/**/*.{html,js,ts,jsx,tsx,scala}",
   ],
   theme: {
     extend: {},
